8ed2b9440dee0703ffaee1f194413cab79b0593e
sebaegana <sebaegana@gmail.com>
2026-01-15 19:58:39 -0300

    Initial commit

---
+++ example_libraries.R
@@ -0,0 +1,7 @@
+## se instala una vez*
+#install.packages("survival")
+## se importa cada vez
+
+library(survival)
+
+Surv(5, 1)
---
+++ script_01.R
@@ -0,0 +1,20 @@
+## Comentario 1 línea
+## Comentario multilínea
+##
+
+x = 2
+y = 5
+xy = 'Hey'
+
+xy = "Hey"
+
+hola = 1 + 1 - 10
+
+hola = "sebastian"
+
+## Parametros por defecto
+
+print(paste(x, y, xy))
+
+print(paste(x, y, xy, sep = ""))
+
